fa635799802e7c8bcd92342657d8d5a200f73bf3
k1rnic <knikolskiy.io@gmail.com>
2021-12-03 23:05:47 +0300

    feat(app): add base packages

---
+++ .storybook/preview.js
@@ -0,0 +1,18 @@
+export const parameters = {
+  layout: 'fullscreen',
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <Box height="100vh" width="100%">
+      <Story />
+    </Box>
+  ),
+];
